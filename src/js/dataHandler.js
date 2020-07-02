32448e167f0f3d6097a6943155b531f50340f030
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-07-02 17:04:54 -0500

    begin to make the logic

---
+++ src/js/dataHandler.js
@@ -0,0 +1,9 @@
+const dataHandler = (data)=>{
+    const selector   = document.getElementById; 
+    const weather    = document.getElementById;
+    const temp       = document.getElementById;
+    const temp_max   = document.getElementById;
+    const temp_min   = document.getElementById;
+    const temp_feels = document.getElementById;
+    const country    = document.getElementById; 
+}
